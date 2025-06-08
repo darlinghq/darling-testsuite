743edeb0888634a1a4f77e826885493bfe6d70a0
Thomas A <curioustommy@protonmail.com>
2025-06-07 19:52:09 -0700

    Add i386 Support For Syscalls

--- kernel/xnu_syscall/include/syscall/syscall.h
+++ kernel/xnu_syscall/include/syscall/syscall.h
@@ -16,6 +16,8 @@
 typedef uint64_t register_size_t;
 #elif defined(__x86_64__)
 typedef uint64_t register_size_t;
+#elif defined(__i386__)
+typedef uint32_t register_size_t;
 #endif
 
 typedef struct {
@@ -23,20 +25,56 @@ typedef struct {
     register_size_t carry_flag;
 } syscall_return_args_t;
 
-void call_syscall(
+#if defined(__i386__)
+void call_sysenter_syscall(
     register_size_t arg1, register_size_t arg2, register_size_t arg3, register_size_t arg4,
     register_size_t arg5, register_size_t arg6, register_size_t arg7, register_size_t arg8,
     int sysnum, syscall_return_args_t* retargs
 );
 
-#define CALL_SYSCALL_ARG0(sysnum, retargs) call_syscall(0, 0, 0, 0, 0, 0, 0, 0, sysnum, retargs)
-#define CALL_SYSCALL_ARG1(sysnum, retargs, arg1) call_syscall(arg1, 0, 0, 0, 0, 0, 0, 0, sysnum, retargs)
-#define CALL_SYSCALL_ARG2(sysnum, retargs, arg1, arg2) call_syscall(arg1, arg2, 0, 0, 0, 0, 0, 0, sysnum, retargs)
-#define CALL_SYSCALL_ARG3(sysnum, retargs, arg1, arg2, arg3) call_syscall(arg1, arg2, arg3, 0, 0, 0, 0, 0, sysnum, retargs)
-#define CALL_SYSCALL_ARG4(sysnum, retargs, arg1, arg2, arg3, arg4) call_syscall(arg1, arg2, arg3, arg4, 0, 0, 0, 0, sysnum, retargs)
-#define CALL_SYSCALL_ARG5(sysnum, retargs, arg1, arg2, arg3, arg4, arg5) call_syscall(arg1, arg2, arg3, arg4, arg5, 0, 0, 0, sysnum, retargs)
-#define CALL_SYSCALL_ARG6(sysnum, retargs, arg1, arg2, arg3, arg4, arg5, arg6) call_syscall(arg1, arg2, arg3, arg4, arg5, arg6, 0, 0, sysnum, retargs)
-#define CALL_SYSCALL_ARG7(sysnum, retargs, arg1, arg2, arg3, arg4, arg5, arg6, arg7) call_syscall(arg1, arg2, arg3, arg4, arg5, arg6, arg7, 0, sysnum, retargs)
-#define CALL_SYSCALL_ARG8(sysnum, retargs, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8) call_syscall(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, sysnum, retargs)
+#define CALL_SYSENTER_SYSCALL_ARG0(sysnum, retargs) call_sysenter_syscall(0, 0, 0, 0, 0, 0, 0, 0, sysnum, retargs)
+#define CALL_SYSENTER_SYSCALL_ARG1(sysnum, retargs, arg1) call_sysenter_syscall(arg1, 0, 0, 0, 0, 0, 0, 0, sysnum, retargs)
+#define CALL_SYSENTER_SYSCALL_ARG2(sysnum, retargs, arg1, arg2) call_sysenter_syscall(arg1, arg2, 0, 0, 0, 0, 0, 0, sysnum, retargs)
+#define CALL_SYSENTER_SYSCALL_ARG3(sysnum, retargs, arg1, arg2, arg3) call_sysenter_syscall(arg1, arg2, arg3, 0, 0, 0, 0, 0, sysnum, retargs)
+#define CALL_SYSENTER_SYSCALL_ARG4(sysnum, retargs, arg1, arg2, arg3, arg4) call_sysenter_syscall(arg1, arg2, arg3, arg4, 0, 0, 0, 0, sysnum, retargs)
+#define CALL_SYSENTER_SYSCALL_ARG5(sysnum, retargs, arg1, arg2, arg3, arg4, arg5) call_sysenter_syscall(arg1, arg2, arg3, arg4, arg5, 0, 0, 0, sysnum, retargs)
+#define CALL_SYSENTER_SYSCALL_ARG6(sysnum, retargs, arg1, arg2, arg3, arg4, arg5, arg6) call_sysenter_syscall(arg1, arg2, arg3, arg4, arg5, arg6, 0, 0, sysnum, retargs)
+#define CALL_SYSENTER_SYSCALL_ARG7(sysnum, retargs, arg1, arg2, arg3, arg4, arg5, arg6, arg7) call_sysenter_syscall(arg1, arg2, arg3, arg4, arg5, arg6, arg7, 0, sysnum, retargs)
+#define CALL_SYSENTER_SYSCALL_ARG8(sysnum, retargs, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8) call_sysenter_syscall(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, sysnum, retargs)
+
+void call_int_unix_syscall(
+    register_size_t arg1, register_size_t arg2, register_size_t arg3, register_size_t arg4,
+    register_size_t arg5, register_size_t arg6, register_size_t arg7, register_size_t arg8,
+    int sysnum, syscall_return_args_t* retargs
+);
+
+#define CALL_INT_UNIX_SYSCALL_ARG0(sysnum, retargs) call_int_unix_syscall(0, 0, 0, 0, 0, 0, 0, 0, sysnum, retargs)
+#define CALL_INT_UNIX_SYSCALL_ARG1(sysnum, retargs, arg1) call_int_unix_syscall(arg1, 0, 0, 0, 0, 0, 0, 0, sysnum, retargs)
+#define CALL_INT_UNIX_SYSCALL_ARG2(sysnum, retargs, arg1, arg2) call_int_unix_syscall(arg1, arg2, 0, 0, 0, 0, 0, 0, sysnum, retargs)
+#define CALL_INT_UNIX_SYSCALL_ARG3(sysnum, retargs, arg1, arg2, arg3) call_int_unix_syscall(arg1, arg2, arg3, 0, 0, 0, 0, 0, sysnum, retargs)
+#define CALL_INT_UNIX_SYSCALL_ARG4(sysnum, retargs, arg1, arg2, arg3, arg4) call_int_unix_syscall(arg1, arg2, arg3, arg4, 0, 0, 0, 0, sysnum, retargs)
+#define CALL_INT_UNIX_SYSCALL_ARG5(sysnum, retargs, arg1, arg2, arg3, arg4, arg5) call_int_unix_syscall(arg1, arg2, arg3, arg4, arg5, 0, 0, 0, sysnum, retargs)
+#define CALL_INT_UNIX_SYSCALL_ARG6(sysnum, retargs, arg1, arg2, arg3, arg4, arg5, arg6) call_int_unix_syscall(arg1, arg2, arg3, arg4, arg5, arg6, 0, 0, sysnum, retargs)
+#define CALL_INT_UNIX_SYSCALL_ARG7(sysnum, retargs, arg1, arg2, arg3, arg4, arg5, arg6, arg7) call_int_unix_syscall(arg1, arg2, arg3, arg4, arg5, arg6, arg7, 0, sysnum, retargs)
+#define CALL_INT_UNIX_SYSCALL_ARG8(sysnum, retargs, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8) call_int_unix_syscall(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, sysnum, retargs)
+
+#else // default
+void call_generic_syscall(
+    register_size_t arg1, register_size_t arg2, register_size_t arg3, register_size_t arg4,
+    register_size_t arg5, register_size_t arg6, register_size_t arg7, register_size_t arg8,
+    int sysnum, syscall_return_args_t* retargs
+);
+
+#define CALL_GENERIC_SYSCALL_ARG0(sysnum, retargs) call_generic_syscall(0, 0, 0, 0, 0, 0, 0, 0, sysnum, retargs)
+#define CALL_GENERIC_SYSCALL_ARG1(sysnum, retargs, arg1) call_generic_syscall(arg1, 0, 0, 0, 0, 0, 0, 0, sysnum, retargs)
+#define CALL_GENERIC_SYSCALL_ARG2(sysnum, retargs, arg1, arg2) call_generic_syscall(arg1, arg2, 0, 0, 0, 0, 0, 0, sysnum, retargs)
+#define CALL_GENERIC_SYSCALL_ARG3(sysnum, retargs, arg1, arg2, arg3) call_generic_syscall(arg1, arg2, arg3, 0, 0, 0, 0, 0, sysnum, retargs)
+#define CALL_GENERIC_SYSCALL_ARG4(sysnum, retargs, arg1, arg2, arg3, arg4) call_generic_syscall(arg1, arg2, arg3, arg4, 0, 0, 0, 0, sysnum, retargs)
+#define CALL_GENERIC_SYSCALL_ARG5(sysnum, retargs, arg1, arg2, arg3, arg4, arg5) call_generic_syscall(arg1, arg2, arg3, arg4, arg5, 0, 0, 0, sysnum, retargs)
+#define CALL_GENERIC_SYSCALL_ARG6(sysnum, retargs, arg1, arg2, arg3, arg4, arg5, arg6) call_generic_syscall(arg1, arg2, arg3, arg4, arg5, arg6, 0, 0, sysnum, retargs)
+#define CALL_GENERIC_SYSCALL_ARG7(sysnum, retargs, arg1, arg2, arg3, arg4, arg5, arg6, arg7) call_generic_syscall(arg1, arg2, arg3, arg4, arg5, arg6, arg7, 0, sysnum, retargs)
+#define CALL_GENERIC_SYSCALL_ARG8(sysnum, retargs, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8) call_generic_syscall(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, sysnum, retargs)
+
+#endif
 
 #endif // DARLING_TESTSUITE_SYSCALL
--- kernel/xnu_syscall/src/syscall/impl/bsd/sys_exit.c
+++ kernel/xnu_syscall/src/syscall/impl/bsd/sys_exit.c
@@ -7,11 +7,15 @@ void sys_exit(int rval) {
     syscall_return_args_t retargs;
     int sysnum;
     
-    #ifdef __x86_64__
+#if defined(__x86_64__)
     sysnum = X86_64_SYSCALL_CLASS_UNIX(SYSNUM_EXIT);
-    #else
+#else
     sysnum = SYSNUM_EXIT;
-    #endif
+#endif
 
-    CALL_SYSCALL_ARG1(sysnum, &retargs, rval);
+#if defined(__i386__)
+    CALL_SYSENTER_SYSCALL_ARG1(sysnum, &retargs, rval);
+#else // default
+    CALL_GENERIC_SYSCALL_ARG1(sysnum, &retargs, rval);
+#endif
 }

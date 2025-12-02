de5fe34b5bd66286c6baccb418805514a65cdcff
Thomas A. <curioustommy@protonmail.com>
2025-12-01 22:07:18 -0800

    Add QuartzCore

---
+++ framework/QuartzCore/test/test_QuartzCore_variable.m
@@ -0,0 +1,9 @@
+#include <QuartzCore/QuartzCore.h>
+
+#include <darling-testsuite/assertion.h>
+
+int main() {
+    // #include <QuartzCore/CAEmitterLayer.h>
+    assert_equals_nsstring(@"point", kCAEmitterLayerPoint);
+    assert_equals_nsstring(@"additive", kCAEmitterLayerAdditive);
+}

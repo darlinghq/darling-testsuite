cb6ba666791990a5c40b38f9e72778fbf25a3408
Thomas A. <curioustommy@protonmail.com>
2025-12-01 22:05:42 -0800

    Add CoreVideo

---
+++ framework/CoreVideo/test/test_CoreVideo_variable.m
@@ -0,0 +1,30 @@
+#include <CoreVideo/CoreVideo.h>
+
+#include <darling-testsuite/assertion.h>
+
+void test_CoreVideo_CVImageBuffer_h();
+
+int main() {
+    test_CoreVideo_CVImageBuffer_h();
+}
+
+void test_CoreVideo_CVImageBuffer_h() {
+    assert_equals_cfstringref(CFSTR("CVPixelAspectRatio"), kCVImageBufferPixelAspectRatioKey);
+    assert_equals_cfstringref(CFSTR("CVImageBufferGammaLevel"), kCVImageBufferGammaLevelKey);
+    assert_equals_cfstringref(CFSTR("CVImageBufferYCbCrMatrix"), kCVImageBufferYCbCrMatrixKey);
+    assert_equals_cfstringref(CFSTR("CVImageBufferColorPrimaries"), kCVImageBufferColorPrimariesKey);
+    assert_equals_cfstringref(CFSTR("CVImageBufferTransferFunction"), kCVImageBufferTransferFunctionKey);
+
+    assert_equals_cfstringref(CFSTR("HorizontalSpacing"), kCVImageBufferPixelAspectRatioHorizontalSpacingKey);
+    assert_equals_cfstringref(CFSTR("VerticalSpacing"), kCVImageBufferPixelAspectRatioVerticalSpacingKey);
+
+    assert_equals_cfstringref(CFSTR("ITU_R_709_2"), kCVImageBufferYCbCrMatrix_ITU_R_709_2);
+    assert_equals_cfstringref(CFSTR("ITU_R_601_4"), kCVImageBufferYCbCrMatrix_ITU_R_601_4);
+    assert_equals_cfstringref(CFSTR("SMPTE_240M_1995"), kCVImageBufferYCbCrMatrix_SMPTE_240M_1995);
+    assert_equals_cfstringref(CFSTR("ITU_R_709_2"), kCVImageBufferColorPrimaries_ITU_R_709_2);
+    assert_equals_cfstringref(CFSTR("EBU_3213"), kCVImageBufferColorPrimaries_EBU_3213);
+    assert_equals_cfstringref(CFSTR("SMPTE_C"), kCVImageBufferColorPrimaries_SMPTE_C);
+    assert_equals_cfstringref(CFSTR("ITU_R_709_2"), kCVImageBufferTransferFunction_ITU_R_709_2);
+    assert_equals_cfstringref(CFSTR("SMPTE_240M_1995"), kCVImageBufferTransferFunction_SMPTE_240M_1995);
+    assert_equals_cfstringref(CFSTR("UseGamma"), kCVImageBufferTransferFunction_UseGamma);
+}

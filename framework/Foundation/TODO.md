2b2ceb5c250e3b7220fbff47652b95879b91c0f3
Thomas A. <curioustommy@protonmail.com>
2025-12-04 20:57:39 -0800

    [Foundation] Update TODO & Add Test Case For kCFTimeZoneSystemTimeZoneDidChangeNotification

---
+++ framework/Foundation/TODO.md
@@ -0,0 +1,2 @@
+* NSAppleEventManager
+  * [`setEventHandler:andSelector:forEventClass:andEventID:`](https://developer.apple.com/documentation/foundation/nsappleeventmanager/seteventhandler(_:andselector:foreventclass:andeventid:)?language=objc)
--- framework/Foundation/test/test_Foundation_variable.m
+++ framework/Foundation/test/test_Foundation_variable.m
@@ -6,6 +6,9 @@ int main() {
     // #include <Foundation/NSConnection.h>
     assert_equals_nsstring(@"NSFailedAuthenticationException", NSFailedAuthenticationException);
 
+    // #include <Foundation/NSTimeZone.h>
+    assert_equals_nsstring(@"kCFTimeZoneSystemTimeZoneDidChangeNotification", NSSystemTimeZoneDidChangeNotification);
+
     // #include <Foundation/NSURL.h>
     assert_equals_nsstring(@"NSURLUbiquitousItemDownloadingStatusNotDownloaded", NSURLUbiquitousItemDownloadingStatusNotDownloaded);
 }

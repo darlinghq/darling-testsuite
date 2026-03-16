e198f642705ee6eccda559b82cad19620ee6e7e0
Thomas A. <curioustommy@protonmail.com>
2026-03-15 20:28:25 -0700

    [SystemConfiguration] Add "kSCDynamicStoreUseSessionKeys"

--- testsuite/System/Library/Frameworks/SystemConfiguration.framework/test/test_SystemConfiguration_variable.m
+++ testsuite/System/Library/Frameworks/SystemConfiguration.framework/test/test_SystemConfiguration_variable.m
@@ -6,6 +6,9 @@
 #include <darling-testsuite/assertion.h>
 
 int main() {
+    // #include <SystemConfiguration/SCDynamicStore.h>
+    assert_equals_CFStringRef(CFSTR("UseSessionKeys"), kSCDynamicStoreUseSessionKeys);
+
     // include <SystemConfiguration/SCSchemaDefinitions.h>
     assert_equals_CFStringRef(CFSTR("Interface"), kSCEntNetInterface);
 }

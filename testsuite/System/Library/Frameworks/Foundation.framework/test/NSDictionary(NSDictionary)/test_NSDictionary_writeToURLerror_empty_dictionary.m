8a305c28c64fb83cb9538d995faf2b035b9d7e9b
Thomas A. <curioustommy@protonmail.com>
2026-06-14 12:25:42 -0700

    [Foundation] Update Test Cases To Support Resource Path

--- testsuite/System/Library/Frameworks/Foundation.framework/test/NSDictionary(NSDictionary)/test_NSDictionary_writeToURLerror_empty_dictionary.m
+++ testsuite/System/Library/Frameworks/Foundation.framework/test/NSDictionary(NSDictionary)/test_NSDictionary_writeToURLerror_empty_dictionary.m
@@ -2,26 +2,34 @@
 // SPDX-License-Identifier: MIT-0
 
 #include <darling-testsuite/assertion.h>
+#include <darling-testsuite/resource.h>
 #include <darling-testsuite/xml.h>
 
 #include <Foundation/Foundation.h>
 
 #include <assert.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void) {
+    const char* expected_relative_path = "testsuite/System/Library/Frameworks/Foundation.framework/resources/expected_writeToURL_error_empty_dict";
+    NSString *generated_path = @"/tmp/generated_writeToURL_error_empty_dict";
     NSError* error;
 
     // Setup
+    unlink([generated_path UTF8String]);
     NSMutableDictionary* mutDict = [[NSMutableDictionary alloc] init];
-    NSURL* url = [NSURL fileURLWithPath:@"generated_writeToURL_error_empty_dict"];
+    NSURL* url = [NSURL fileURLWithPath:generated_path];
     
     // Execute
     BOOL isSuccessful = [mutDict writeToURL:url error:&error];
     assert_NSError_not_set(error, !isSuccessful);
 
     // Verify
-    if (!is_xml_equal("expected_writeToURL_error_empty_dict", "generated_writeToURL_error_empty_dict")) {
+    const char *expected_path = grab_full_resource_path(expected_relative_path);
+    if (!is_xml_equal(expected_path, [generated_path UTF8String])) {
         abort();
     }
+
+    free((void*)expected_path);
 }

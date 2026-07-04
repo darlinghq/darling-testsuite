c928554d24c4b905611b4861a93f64b7758c2f66
Thomas A. <curioustommy@protonmail.com>
2026-07-04 13:23:28 -0700

    [darling-testsuite] Create Container For Handling Resource Paths

--- lib/darling-testsuite/src/darling-testsuite/resource.m
+++ lib/darling-testsuite/src/darling-testsuite/resource.m
@@ -3,26 +3,66 @@
 
 #include <darling-testsuite/resource.h>
 
+#include <darling-testsuite/algorithm.h>
+
 #include <sys/syslimits.h>
 
 #include <dirent.h>
 #include <stdlib.h>
 #include <string.h>
 
-char *grab_base_resource_path();
+struct opaque_resource_container {
+    const char* base_path;
+    linked_list_t *allocated_resource_paths;
+};
 
-const char *grab_full_resource_path(const char *resource_path) {
-    char *base_path = grab_base_resource_path();
-    if (base_path == NULL || resource_path == NULL) {
+const char *grab_base_resource_path();
+resource_container_pt alloc_resource_container();
+void free_allocated_resource_path(void* ptr);
+
+resource_container_pt resource_container_alloc() {
+    return malloc(sizeof(struct opaque_resource_container));
+}
+
+resource_container_pt resource_container_init() {
+    const char *base_path = grab_base_resource_path();
+
+    if (base_path == NULL) {
+        return NULL;
+    }
+    
+    resource_container_pt container = resource_container_alloc();
+    container->base_path = base_path;
+    container->allocated_resource_paths = NULL;
+    return container;
+}
+
+void resource_container_free(resource_container_pt* container) {
+    linked_list_freeall(&(*container)->allocated_resource_paths, free_allocated_resource_path);
+    free(*container);
+    *container = NULL;
+}
+
+void free_allocated_resource_path(void* ptr) {
+    free(ptr);
+}
+
+const char *grab_full_resource_path(resource_container_pt container, const char *resource_path) {
+    if (resource_path == NULL) {
         return NULL;
     }
 
+    const char *base_path = container->base_path;
     size_t base_length = strlen(base_path);
+
     size_t divider_length = base_path[base_length-1] == '/' ? 0 : 1;
+
     size_t relative_path_length = strlen(resource_path);
-    size_t new_str_length = base_length + divider_length + relative_path_length + sizeof('\0');
     
-    char *full_path = reallocf(base_path, new_str_length);
+    size_t new_str_length = base_length + divider_length + relative_path_length + sizeof('\0');
+    char *full_path = malloc(new_str_length);
+
+    memcpy(full_path, base_path, base_length);
     if (divider_length > 0) {
         full_path[base_length] = '/';
         full_path[base_length+1] = '\0';
@@ -30,15 +70,16 @@ const char *grab_full_resource_path(const char *resource_path) {
         full_path[base_length] = '\0';
     }
 
+    linked_list_append_next(&container->allocated_resource_paths, full_path);
     return strncat(full_path, resource_path, new_str_length);
 }
 
-NSString *grab_full_resource_path_nsstring(NSString* resource_path) {
+NSString *grab_full_resource_path_nsstring(resource_container_pt container, NSString* resource_path) {
     if (resource_path == nil) {
         return nil;
     }
 
-    const char *cstring_result = grab_full_resource_path([resource_path UTF8String]);
+    const char *cstring_result = grab_full_resource_path(container, [resource_path UTF8String]);
     if (cstring_result == NULL) {
         return nil;
     }
@@ -46,11 +87,11 @@ NSString *grab_full_resource_path_nsstring(NSString* resource_path) {
     NSString* result = [[NSString alloc] initWithBytesNoCopy:(void *)cstring_result 
                                                       length:strlen(cstring_result)
                                                     encoding:NSUTF8StringEncoding
-                                                freeWhenDone:YES];
+                                                freeWhenDone:NO];
     return [result autorelease];
 }
 
-char *grab_base_resource_path() {
+const char *grab_base_resource_path() {
     const char *paths_to_check[] = {
         getenv("DARLING_TESTSUITE_RESOURCE_PATH"),
         "resource",
@@ -63,7 +104,7 @@ char *grab_base_resource_path() {
         DIR *dir = opendir(path);
         if (dir != NULL) {
             closedir(dir);
-            return strdup(path);
+            return path;
         }
     }
 

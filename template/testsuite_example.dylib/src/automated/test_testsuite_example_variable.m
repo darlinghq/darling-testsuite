5d4c2af59d95334db1596a8162103bc0024348d5
Thomas A. <curioustommy@protonmail.com>
2026-09-05 15:20:55 -0700

    Upload testsuite_example.dylib template

---
+++ template/testsuite_example.dylib/src/automated/test_testsuite_example_variable.m
@@ -0,0 +1,22 @@
+// SPDX-FileCopyrightText: 2026 Darling Team
+// SPDX-License-Identifier: MIT-0
+
+// #include <Example/Example.h>
+
+#include <darling-testsuite/assertion.h>
+#include <darling-testsuite/testcase.h>
+
+#define TESTCASE_TYPE TESTCASE_TYPE_AUTOMATED
+
+// Testcase Logic
+#include <darling-testsuite/testcase/setup.h>
+
+void setup_testcase(testsuite_container_pt context) {}
+void cleanup_testcase(testsuite_container_pt context) {}
+
+int execute_testcase(testsuite_container_pt context, int argc, char **argv) {
+    // #include <Example/Variables.h>
+    // assert_equals_NSString(@"Example", kExampleVar);
+
+    return 0;
+}
